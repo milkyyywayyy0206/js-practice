d0122f3d3dd2fd6208a252a80b748ea7c05a4b7c
DE GUIA, MARK DANIEL C. <markdanieldeguia0@gmail.com>
2025-02-20 22:05:18 +0800

    Add files via upload

---
+++ LA1_DE GUIA_CS1A/variables.js
@@ -0,0 +1,16 @@
+// 1. Create variable name (name) & store your name.
+let name = 'De Guia, Mark Daniel C.';
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+let whatDoYouWannaBecomeInYourLife = "programmer";
+// 3. Create variable name (gender) & store your gender.
+let gender = 'male';
+// 4. Create variable name (facebookAcct) & store your Facebook Name.
+let facebookAcct = 'Mark Daniel De Guia';
+
+// 5. Finally log all variables to the console using code runner in vscode.
+console.log('My name is ' + name);
+console.log('I want to become a ' + whatDoYouWannaBecomeInYourLife);
+console.log('gender: ' + gender);
+console.log('my facebook account: ' + facebookAcct);
+
+// 6. Submit answers in your github repository and link it to your google classroom.
